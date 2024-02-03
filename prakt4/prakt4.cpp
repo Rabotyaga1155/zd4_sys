c787f1ef65a596e967938f5574eb659a845b888d
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2024-02-03 13:24:27 +0500

    Add files via upload

---
+++ prakt4/prakt4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    ifstream infile("data.txt");
+    ofstream outfile("Rezultat.txt", ios::app);
+
+    if (!infile.is_open()) {
+        cout << "Ошибка открытия файла data.txt" << endl;
+        return 1;
+    }
+
+    if (!outfile.is_open()) {
+        cout << "Ошибка открытия файла Rezultat.txt" << endl;
+        return 1;
+    }
+
+    double a, b, alpha;
+
+    while (infile >> a >> b >> alpha) {
+        outfile << "Сторона квадрата а -" << a << " " << "Сторона квадрата b - " << b << endl;
+    }
+
+    infile.close();
+    outfile.close();
+
+    return 0;
+}
